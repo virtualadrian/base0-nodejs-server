fad242b2be24bd39757730efdd8de64c90ef2c4d
mohankumaranna <armk100@gmail.com>
2016-08-05 11:07:10 +0530

    adding login feature

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## base0 nodejs server
 
 A base server to build server side code in  
+  
 	- Javascript  
   
 	- MySQL Database  
--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var _ 			= require('lodash'),
 	bodyParser 	= require('body-parser'),
 	User 		= require('./models/user'),
 	Bookshelf 	= require('./config/database'),
-	cors		= require('cors');
+	cors		= require('cors'),
+	jwt			= require('jsonwebtoken'),
+	constants	= require('./config/constants');
 
 var	app 		= express();
 // body-parser middleware for handling request variables
@@ -23,12 +25,36 @@ router.route('/users/:id').get(user_obj); // fetch an user's details
 router.route('/users/:id').put(update_user_obj); // update user's details
 router.route('/users/:id').delete(delete_user_obj); // delete an user
 
+router.route('/sessions/create').post(login_user);
+
 app.use('/api', router);
 
 // Launch NodeJS server with port #3002
 app.listen(3002, function(){
 	console.log('Express server listening on port %d in %s mode',  3002, 'development');
 });
+
+// private functions
+var retrieved_users = null;  // yet to retrieve users
+
+(function retrieve_users(){
+	console.log('Retrieve User List');
+	Users.forge().fetch()
+		.then(user_fetch_success)
+		.catch(user_fetch_error);
+//	var result = {error:true, data:{message: 'Incomplete Retrieve'}};
+
+	function user_fetch_success(collection){
+		console.log('User Fetch Success...');
+		retrieved_users = { error: false, data: collection.toJSON() };
+	}
+	function user_fetch_error(err){
+		console.log('User error...');
+		console.log(err);
+		retrieved_users = { error: true, data: {message: err.message} };
+	}
+//	return result;
+})();
 
 
 // functions referred in router above
@@ -50,10 +76,12 @@ function user_list(request, response){
 	}
 
 }
+
 function new_user(request, response){
-console.log('New user being added...');
-console.log(request.body);
-console.log(request.query);
+
+	console.log('New user being added...');
+	console.log(request.body);
+	console.log(request.query);
 
 	User.forge({
 		name: request.body.userName,
@@ -143,3 +171,44 @@ function delete_user_obj(request, response){
 		response.status(500).json({error: true, data: {message: err.message}});
 	}
 }
+
+function create_token(user){
+	return jwt.sign(_.omit(user, 'password'), constants.secret, {expiresIn: 60*60*2});
+}
+
+function login_user(request, response){
+	console.log('Login user...');
+	console.log('Email Received as: '+request.body.email);
+	console.log('Password Received as: '+request.body.password);
+
+	if( !request.body.email || !request.body.password){
+		console.log('Email and Password needed....');
+		return response.status(400).send("Email and Password needed");
+	}
+	
+	var user;
+	console.log('Retrived Users data: ');
+	console.log(retrieved_users.data);
+
+	if(retrieved_users.error){
+		return response.status(400).send("Error in User List");	
+	}
+	user = _.find(retrieved_users.data, {email: request.body.email});
+
+	console.log('User object:');
+	console.log(user);
+
+	if(!user){
+		console.log("Email or Password do not match");
+		return response.status(401).send("Email or Password do not match");
+	}
+	if(user.password !== request.body.password){
+		console.log("Email or Password don't match");
+		return response.status(401).send("Email or Password don't match");
+	}
+	console.log('Login success...');
+	response.status(201).send({
+		id_token: create_token(user)
+	})
+}
+
